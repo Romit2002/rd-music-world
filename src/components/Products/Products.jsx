70c845a7198e0aa9a74da16d1f1d9014ad46cdf4
Romit <romit3766@gmail.com>
2023-07-18 02:13:36 +0530

    added product and products component

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,25 @@
+import { Fragment } from "react";
 import "./Products.scss";
+import Product from './Product/Product';
 const Products = () => {
-    return <div>Products</div>;
+    return(
+        <Fragment>
+            <div className="products-container">
+                <div className="sec-heading">Section Heading
+                </div>
+                <div className="products">
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                    <Product />
+                </div>
+            </div>
+        </Fragment>
+    )
 };
 
 export default Products;
